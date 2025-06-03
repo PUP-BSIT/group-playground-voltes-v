bd34ed499920f51c5cfa921fdfa7c6183e0a4de1
Morales-WomanDaphne <moraleswomandaphne@gmail.com>
2025-06-03 22:05:41 +0800

    morales test

--- voltes/corpus.py
+++ voltes/corpus.py
@@ -42,6 +42,7 @@ def corpus_menu():
                 print("-----------------------------------------------------")
             case 5:
                 print("Comment From Morales")
+                print("Makakapasa ka sa INTE 202")
                 print("-----------------------------------------------------")
             case 6:
                 print("Comment From Pineda")
